ef87e1f5127f09886020fcbace02e64768bd2972
Nitrow91 <printz.a91@gmail.com>
2020-09-21 19:07:57 +0200

    Cleaned up master a bit

--- P9/controllers/master/master.py
+++ P9/controllers/master/master.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 import gym
 import P9_RL_env_v01
-import random
 import numpy as np
-import time
 import os
 import tensorflow as tf
 
@@ -15,12 +13,8 @@ policy_kwargs = dict(act_fun=tf.nn.tanh, net_arch=[128, 128])
 from stable_baselines import SAC
 from stable_baselines import PPO1
 
-def read_file(filename):
-    with open(filename) as fp:
-        return int(fp.read())
-
 # Make sure to increment this with each run
-simRunID = "PPO_256_256V5"
+simRunID = "PPO_256_256"
 
 # Creating file structure
 simLogPath = "Logging/"+simRunID+"/"
@@ -30,21 +24,18 @@ if not os.path.exists(simLogPath): #Create directories
     os.makedirs(simLogPath + 'screenshots/timeout', exist_ok=True)
     os.makedirs(simLogPath + 'screenshots/success', exist_ok=True)
     print('Directory structure created...')
-#simRunTime = time.strftime("%Y-%m-%d", time.gmtime()) # File name
-simName = 'PPO_256_256V5'
+
 epFile = simLogPath + 'log/eps.txt'
-timeStepsToTrain = 50000000
+timeStepsToTrain = 1000000
 tensorboardPath = simLogPath + 'P7v1_tensorboard/'
 
 env = gym.make('P9_RL-v0')
 print('Environment created...')
-# Check how many episodes the model has been trained for:
 
-#  Check if training log of previous training exists, load model if does
-#model = PPO1.load(simLogPath +'models/'+ simName, env, tensorboard_log=tensorboardPath)
 model = PPO1("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1, gamma=0.99, tensorboard_log=tensorboardPath)
+#model = PPO1.load(simLogPath +'models/'+ simName, env, tensorboard_log=tensorboardPath) 
 model.learn(total_timesteps=timeStepsToTrain, log_interval=1)
 print('Training finished...')
-model.save(simLogPath +'models/'+ simName)
+model.save(simLogPath +'models/'+ simRunID)
 print('Model saved...')
 env.supervisor.simulationSetMode(0)
